e9ceb57a84275c2076ee5bb1e2ddec8eee1b4d48
Mateusz@MSI <matkac98@gmail.com>
2020-12-13 12:33:04 +0100

    Zadanie 7

--- petle.py
+++ petle.py
@@ -1,5 +1,10 @@
-for x in range (1,8):
-    if (x==5):
-        print("Znalazłem 5 !")
-        continue
-    print (x)
+x=['1','2','3']
+y=['a','b','c']
+a=0
+b=0
+for i in x:
+    for j in y:
+        print (x[a],y[b])
+        b+=1
+    a+=1
+    b=0
