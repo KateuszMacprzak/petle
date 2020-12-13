548f2a08cda8fa460c26f4e3c7c09540caf0c0db
Mateusz@MSI <matkac98@gmail.com>
2020-12-13 12:04:39 +0100

    Zadanie 2

--- petle.py
+++ petle.py
@@ -1,2 +1,13 @@
-for x in range(0,5,1):
-    print(x)
+def funkcja(a,b):
+    a=int(a)
+    b=int(b)
+    z=a+b
+    y=a-b
+    k=a*b
+    p=a**3
+    d=a//3
+    print(z,y,k,p,d)
+print("Witaj w programie")
+x=input("Wprowadz pierwszą liczbę: ")
+y=input("Wprowadz drugą liczbę: ")
+funkcja(x,y)
