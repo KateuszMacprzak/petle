e01a665f63c5ffbd27fcf61fd47e5ae2500654ad
Mateusz@MSI <matkac98@gmail.com>
2020-12-14 17:33:56 +0100

    Zadanie_56*

--- petle.py
+++ petle.py
@@ -1,27 +1,15 @@
-x='ARA'
-y='BereKA'
-z='GONabEACH'
-print('Wybierz numer wyrazu, który ma być przeliterowany ? ',x,y,z)
-a=input('Podaj liczbę 1, 2 lub 3: ')
-a=int(a)
-if (a==1):
-        for i in x:
-            print(i)
-if (a==2):
-        for i in y:
-            print(i)
-if (a==3):
-        for i in z:
-            print(i)
-        
-#rozwiązanie ze strony internetowej
-#a = ('ARA','BereKA','GONabEACH')
-#print("To są wyrazy do wyboru:",a[0],",",a[1],",",a[2])
-#
-#n = int(input('''Wybierz numer wyrazu, który ma być przeliterowany? 
-#    Podaj liczbę: 1, 2 lub 3: '''))
-#n -=1
-#print("wybrałeś słowo:",a[n])
-#for s in a[n]:
-#    print(s)         
+a=('ARA','BereKA','GONabEACH')
+n=0
+y=-1
+for x in a:
+   n+=1
+   y+=1
+   print(n,a[y]) 
 
+#rozwiązanie ze strony
+#!!!ZWRÓCIĆ UWAGĘ NA FORMUŁĘ!!!
+#a = ('ARA','BereKA','GONabEACH')
+#print("To są wyrazy do wyboru:")
+## Formuła, która umożliwia wyświetlenie indexów z listy
+#for z, g in enumerate(a):
+#        print(z+1, g)
