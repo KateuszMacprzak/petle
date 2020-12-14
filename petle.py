96b34fa1dbd0ff0a17de7bad7c28eb8d41129b14
Mateusz@MSI <matkac98@gmail.com>
2020-12-14 11:07:02 +0100

    Zadanie_24*

--- petle.py
+++ petle.py
@@ -1,8 +1,4 @@
-podsumowanie=0
-for a in range(3):
-    print('Wprowadz wartość: ')
-    nowa_wartosc=input()
-    nowa_wartosc=int(nowa_wartosc)
-    podsumowanie+=nowa_wartosc
-print('Suma wpisanych wartości to: ', podsumowanie)
-    
+r=[['CA','NV','UT'],['NJ','NY','DE']]
+for a in r:
+    for b in a:
+        print(b)
