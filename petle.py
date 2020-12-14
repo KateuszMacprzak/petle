541e4337b5f6069d42a25ca9a35b50a24283d890
Mateusz@MSI <matkac98@gmail.com>
2020-12-14 17:29:29 +0100

    Zadanie_55

--- petle.py
+++ petle.py
@@ -1,10 +1,27 @@
-liczba=input('Wprowadz wartość: ')
-a=1
-liczba=int(liczba)
-if (liczba==0):
-    print('0! wynosi 1 ')
-else:
-    while liczba >=1:
-        a=a*liczba
-        liczba=liczba-1
-    print('Silnia wynosi: ',a)
+x='ARA'
+y='BereKA'
+z='GONabEACH'
+print('Wybierz numer wyrazu, który ma być przeliterowany ? ',x,y,z)
+a=input('Podaj liczbę 1, 2 lub 3: ')
+a=int(a)
+if (a==1):
+        for i in x:
+            print(i)
+if (a==2):
+        for i in y:
+            print(i)
+if (a==3):
+        for i in z:
+            print(i)
+        
+#rozwiązanie ze strony internetowej
+#a = ('ARA','BereKA','GONabEACH')
+#print("To są wyrazy do wyboru:",a[0],",",a[1],",",a[2])
+#
+#n = int(input('''Wybierz numer wyrazu, który ma być przeliterowany? 
+#    Podaj liczbę: 1, 2 lub 3: '''))
+#n -=1
+#print("wybrałeś słowo:",a[n])
+#for s in a[n]:
+#    print(s)         
+
