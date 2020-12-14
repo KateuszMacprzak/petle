1caa6cac4ba93a8cf4751401f0919475aabcb8f4
Mateusz@MSI <matkac98@gmail.com>
2020-12-14 16:00:12 +0100

    Zadanie 45

--- petle.py
+++ petle.py
@@ -1,9 +1,14 @@
-x='HURA!'
-y=0
-a=input('Ile razy chcesz powiedzieć HURA ?: ')
-a=int(a)
-while (a>0):
-    for i in range (0,a,1):
-        print(x)
-    break
-print('Koniec już tego...')
+k=['Agaton','zapiecek','bekon','Renata','Wojtek']
+y=-1
+x=0
+for i in k:
+    y+=1
+    x+=1
+    print(x, k[y])
+
+#rozwiązanie ze strony
+#k = ['Agaton','zapiecek','bekon', 'Renata','Wojtek']
+#k.sort() #sortuje
+#for i, g in enumerate(k):
+#    i+=1 # po to żeby nie zaczynał od zera
+#    print(i, g)
