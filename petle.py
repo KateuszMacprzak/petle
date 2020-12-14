613aa2213d359b6ed38f685c25a37c66e8d7c029
Mateusz@MSI <matkac98@gmail.com>
2020-12-14 15:53:25 +0100

    Zadanie_44

--- petle.py
+++ petle.py
@@ -1,7 +1,9 @@
-n=input('Wprowadz wartość: ')
-n=int(n)
-while n>0:
-        x=((n*(n+1))/2)
-        x=int(x)
-        print('Suma liczb wynosi: ',x)
-        break
+x='HURA!'
+y=0
+a=input('Ile razy chcesz powiedzieć HURA ?: ')
+a=int(a)
+while (a>0):
+    for i in range (0,a,1):
+        print(x)
+    break
+print('Koniec już tego...')
