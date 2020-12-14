0c70ed74fab66eb8fefdc6df31f747d673d8ac3e
Mateusz@MSI <matkac98@gmail.com>
2020-12-14 13:14:43 +0100

    Zadanie_39

--- petle.py
+++ petle.py
@@ -1,11 +1,2 @@
-x = 1
-y = 0
-print('Proszę wprowadzić kolejną wartość.')
-print('Jeżeli wprowadzisz 0 to kończymy procedurę.')
-while x !=0:  # pętla działa dopuki wprowadzany x nie równa się zero
-    print ('Curent Sum',y) #drukuje sume zgodnie z: y=x+y
-    x = float(input('Wprowadź wartość:')) # wprowadzanie wartości
-
-    y += x     # y=x+y
-print ('''Procedura zatrzymana.
-Łączna wartość wprowadzonych liczb to:''', y)
+for i in range(3,20,3):
+        print(i)
