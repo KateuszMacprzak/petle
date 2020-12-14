e101ad9c0d9c3ad31d4ae0bff7b5269d44f2e516
Mateusz@MSI <matkac98@gmail.com>
2020-12-14 10:34:06 +0100

    Zadanie_19*

--- petle.py
+++ petle.py
@@ -1,7 +1,10 @@
-a=input("Wpisz literę n lub c: ")
-if (a=='n'):
-    print ('Dziekuje')
-elif (a=='c'):
-    print ('Dziekuje')
-else:
-    print('To zły wybór ! ')
+def funkcja():
+    a=input('Proszę wcisnąć literę n lub c: ')
+    if (a=='n'):
+        print('Dziękuje !')
+    elif (a=='c'):
+        print('Dziękuje !')
+    elif (a!='n' or a!='c'):
+        funkcja()
+print('Witaj w programie :)')
+funkcja()
