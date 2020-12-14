f640be26e50f31b3e6aadedea491d8c356e286df
Mateusz@MSI <matkac98@gmail.com>
2020-12-14 11:17:15 +0100

    Zadanie_25

--- petle.py
+++ petle.py
@@ -1,4 +1,8 @@
-r=[['CA','NV','UT'],['NJ','NY','DE']]
-for a in r:
-    for b in a:
-        print(b)
+b=0 #ta zmienna ma przechowywać ilość wprowadzonych 5 
+for x in range(6):
+    a=input('Wprowadz liczbe od 1 do 10: ')
+    a=int(a)
+    if (a==5):
+        b+=1
+print('Ilość wprowadzonych 5: ',b)
+    
