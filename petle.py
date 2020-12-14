ee9b0d371290bfa223f5cba8b3b6cef84ba1ce8d
Mateusz@MSI <matkac98@gmail.com>
2020-12-14 17:36:11 +0100

    Zadanie_57

--- petle.py
+++ petle.py
@@ -1,15 +1,5 @@
-a=('ARA','BereKA','GONabEACH')
+k=['salad','pasta','pizza','nachos']
 n=0
-y=-1
-for x in a:
-   n+=1
-   y+=1
-   print(n,a[y]) 
-
-#rozwiązanie ze strony
-#!!!ZWRÓCIĆ UWAGĘ NA FORMUŁĘ!!!
-#a = ('ARA','BereKA','GONabEACH')
-#print("To są wyrazy do wyboru:")
-## Formuła, która umożliwia wyświetlenie indexów z listy
-#for z, g in enumerate(a):
-#        print(z+1, g)
+for i in k:
+    n+=1
+    print(n, i)
