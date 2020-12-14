711c61b6ac8ba908292fe7105090769a938197d3
Mateusz@MSI <matkac98@gmail.com>
2020-12-14 16:52:11 +0100

    Zadanie_53

--- petle.py
+++ petle.py
@@ -1,5 +1,11 @@
-y=0
-y+=6
-while(y<31):
-    print(y)
-    y+=6
+#wykonanie za pomocą pętli while
+#i=0
+#while(i>-1):
+#    i+=1
+#    print('h = ',i)
+#    if(i==6):
+#        break
+
+#za pomocą pętli for 
+for i in range (1,7,1):
+    print('h=',i)
