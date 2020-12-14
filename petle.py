8c0428f503adce4d9314027977f18095920b6922
Mateusz@MSI <matkac98@gmail.com>
2020-12-14 16:07:52 +0100

    Zadanie_46

--- petle.py
+++ petle.py
@@ -1,14 +1,6 @@
-k=['Agaton','zapiecek','bekon','Renata','Wojtek']
-y=-1
-x=0
-for i in k:
-    y+=1
-    x+=1
-    print(x, k[y])
-
-#rozwiązanie ze strony
-#k = ['Agaton','zapiecek','bekon', 'Renata','Wojtek']
-#k.sort() #sortuje
-#for i, g in enumerate(k):
-#    i+=1 # po to żeby nie zaczynał od zera
-#    print(i, g)
+i=0
+while True:
+    print(i)
+    i+=1
+    if (i>=5):
+        break
