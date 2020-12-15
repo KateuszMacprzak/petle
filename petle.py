80f89e8bb3f801a43d189802db59ee8dc892f4dd
Mateusz@MSI <matkac98@gmail.com>
2020-12-16 00:25:54 +0100

    Zadanie_59

--- petle.py
+++ petle.py
@@ -1,10 +1,5 @@
-a=['1','2']
-b=['00','15','30','45']
-x=0
-y=0
-for i in a:
-    for j in b:
-        print(a[x],":",b[y],"PM")
-        y+=1
-    x+=1
-    y=0
+g=['Adrian1','Adrian2','Adrian3','Adrian4']
+z=1
+for i in g:
+    print(z,i)
+    z+=1
