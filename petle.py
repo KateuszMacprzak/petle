67a02149fd619bc02b109103713050d30e54b310
Mateusz@MSI <matkac98@gmail.com>
2020-12-16 00:07:03 +0100

    Zadanie_76-instrukcja

--- petle.py
+++ petle.py
@@ -1,6 +1,10 @@
-x=1
-while (x>0):
-    z=input('Czy podziwiasz Natalinkę moją kochaną ?')
-    if(z=='tak'):
-        print('Dziękuje za szczerą odpowiedz !')
-        break
+a=['1','2']
+b=['00','15','30','45']
+x=0
+y=0
+for i in a:
+    for j in b:
+        print(a[x],":",b[y],"PM")
+        y+=1
+    x+=1
+    y=0
