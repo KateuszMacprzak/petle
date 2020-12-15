465e4474ce5d822dc903dc5b27dc66a4c6e05f12
Mateusz@MSI <matkac98@gmail.com>
2020-12-15 16:36:03 +0100

    Zadanie_69

--- petle.py
+++ petle.py
@@ -1,16 +1,4 @@
-import random
-imie=random.choice(['Ewa','Mateusz','Natalia','Michał'])
-dlugosc=len(imie)
-if (imie[-1]=='a'):
-    print('To imię damskie !')
-else:
-    print('To imię męskie !')
-print(dlugosc)
-a=input('Spróbuj zgadnąć to imię: ')
-x=0
-while(a!=imie):
-    x+=1
-    print('Nie udało Ci się zgadnąć, to Twoja', x, 'próba')
-    a=input('Spróbuj zgadnąć to imie: ')
-else:
-    print('Gratulacje, udało Ci się wygrać ! To była Twoja ',x,' próba')
+a=0
+for x in range (1,5,1):
+    a+=1
+    print('próba nr:',a)
