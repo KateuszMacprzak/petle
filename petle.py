4743008835f885c3a146d34550f2fd5a7d502178
Mateusz@MSI <matkac98@gmail.com>
2020-12-15 17:34:20 +0100

    Zadanie_78

--- petle.py
+++ petle.py
@@ -1,6 +1,5 @@
-def zakres(a,b):
-    a=a+1 #pętla ma się zacząć od wartości zadanej
-    while a > b: # pętla ma się kręcić az ...
-        a = a - 1   #definicja pętli
-        print(a)
-zakres(17,15)
+x=['Ewa','Mateusz','Natalia','Michał']
+y=0
+for i in x:
+    print('Cześć ',x[y],'!',' ','Zapraszam na moje urodziny')
+    y+=1
