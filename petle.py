5b2985c4bf343b6a6bcc84197a724eab6e8aaa76
Mateusz@MSI <matkac98@gmail.com>
2020-12-15 16:42:50 +0100

    Zadanie_70

--- petle.py
+++ petle.py
@@ -1,4 +1,10 @@
-a=0
-for x in range (1,5,1):
-    a+=1
-    print('próba nr:',a)
+def funkcja():
+    for i in range (10,41):
+        if (i%2==0):
+            continue
+        if (i%3==0):
+            continue
+        if (i%5==0):
+            continue
+        print(i)
+funkcja()
