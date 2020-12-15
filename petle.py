cf4b9e1a268b2e971adcc4106ff132cae67db5d4
Mateusz@MSI <matkac98@gmail.com>
2020-12-15 23:41:57 +0100

    Moja pierwsza próba-38

--- petle.py
+++ petle.py
@@ -1,5 +1,10 @@
-x=5
-while(x>-1):
-    print(x)
-    x-=1
-    
+x=1
+y=0
+y=int(y)
+while (x>0):
+    a=input('Prosze podać kolejną wartość: ')
+    a=int(a)
+    y+=a
+    print('Obecna suma wynosi: ',y)
+    if(a==0):
+        print('Koniec działań. Wynik wynosi: ',y)
