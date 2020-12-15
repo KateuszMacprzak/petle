717b45e050db5f1ca6c01b9b8b9de316485f7b1f
Mateusz@MSI <matkac98@gmail.com>
2020-12-15 17:26:30 +0100

    Zadanie_78*

--- petle.py
+++ petle.py
@@ -1,6 +1,6 @@
-x=2
-while(x>1):
-    if(x==7):
-        break
-    x+=1
-    print(x)
+def zakres(a,b):
+    a=a+1 #pętla ma się zacząć od wartości zadanej
+    while a > b: # pętla ma się kręcić az ...
+        a = a - 1   #definicja pętli
+        print(a)
+zakres(17,15)
