c354fcde024ce6f4631b30a6a74375bb6efd1393
Mateusz@MSI <matkac98@gmail.com>
2020-12-15 23:18:06 +0100

    Zadanie_24-instrukcja

--- petle.py
+++ petle.py
@@ -1,13 +1,4 @@
-x=input('Proszę wpisać literę n lub c: ')
-i=1
-def funkcja():
-    while (i>0):
-        a=input('Proszę wpisać literę n lub c: ')
-        if (a=="n"):
-            print("Dziękuje")
-            break
-if (x=="n" or x=="c"):
-    print("Dziękuje")
-else:
-    print("Błąd")
-    funkcja()
+R = [["CA","NV","UT"], ["NJ","NY","DE"]]
+for a in R: #dla elementów w pętli R
+    for b in a: #dla elementu w a
+        print(b) #wyświetl każdy element
