d8fff8d0773b7da3c4f5c1aeb5f7ae8d7e255db3
Mateusz@MSI <matkac98@gmail.com>
2020-12-15 17:09:34 +0100

    Zadanie_77-petla_for

--- petle.py
+++ petle.py
@@ -1,10 +1,2 @@
-x=['1','2']
-y=['00','15','30','45']
-a=0
-b=0
-for i in x:
-    for j in y:
-        print(x[a],':',y[b], 'PM')
-        b+=1
-    a+=1
-    b=0
+for i in range(3,8):
+    print(i)
