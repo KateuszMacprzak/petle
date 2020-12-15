b9a545763fc16b0197975e0d1b46a8727c655cad
Mateusz@MSI <matkac98@gmail.com>
2020-12-15 16:33:58 +0100

    Zadanie_68

--- petle.py
+++ petle.py
@@ -1,4 +1,16 @@
-a=input('Czy podziwiasz mądrość Natalii ?: ')
-while (a!='tak'):
-    print('Dziwię Ci się !')
-    break
+import random
+imie=random.choice(['Ewa','Mateusz','Natalia','Michał'])
+dlugosc=len(imie)
+if (imie[-1]=='a'):
+    print('To imię damskie !')
+else:
+    print('To imię męskie !')
+print(dlugosc)
+a=input('Spróbuj zgadnąć to imię: ')
+x=0
+while(a!=imie):
+    x+=1
+    print('Nie udało Ci się zgadnąć, to Twoja', x, 'próba')
+    a=input('Spróbuj zgadnąć to imie: ')
+else:
+    print('Gratulacje, udało Ci się wygrać ! To była Twoja ',x,' próba')
